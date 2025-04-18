f2502f12f910eeda204636441c74cda91490b04d
Filip Hládek <71432962+Deam0on@users.noreply.github.com>
2025-04-18 14:57:29 +0200

    cuda

--- data_preparation.py
+++ data_preparation.py
@@ -3,7 +3,7 @@ import json
 import os
 import random
 from pathlib import Path
-
+import torch
 import numpy as np
 import shapely.affinity
 import shapely.geometry
@@ -264,8 +264,8 @@ def choose_and_use_model(model_paths, dataset_name, threshold):
             "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
         )
     )
-    # cfg.MODEL.DEVICE = "cuda"
-    cfg.MODEL.DEVICE = "cpu"
+    
+    cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
 
     predictor = load_model(cfg, model_path, dataset_name)
