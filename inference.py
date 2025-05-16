749bbe2cdf6d4b2a3ba9c93e746e7b018a222802
Filip Hládek <71432962+Deam0on@users.noreply.github.com>
2025-05-16 12:53:58 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -613,30 +613,30 @@ def run_inference(dataset_name, output_dir, visualize=False, threshold=0.65):
                 # Use canny edge detection
                 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-                reader = easyocr.Reader(['en'])
-                result = reader.readtext(gray, detail=0, paragraph=False, contrast_ths=0.85, adjust_contrast=0.85, add_margin=0.25, width_ths=0.25, decoder='beamsearch')
-                if result:
-                    pxum_r = result[0]
-                    psum = re.sub("[^0-9]", "", pxum_r)
-                else:
-                    pxum_r = ''
-                    psum = '0'
+                # reader = easyocr.Reader(['en'])
+                # result = reader.readtext(gray, detail=0, paragraph=False, contrast_ths=0.85, adjust_contrast=0.85, add_margin=0.25, width_ths=0.25, decoder='beamsearch')
+                # if result:
+                #     pxum_r = result[0]
+                #     psum = re.sub("[^0-9]", "", pxum_r)
+                # else:
+                #     pxum_r = ''
+                #     psum = '0'
 
-                lines_list = []
-                lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=1)
+                # lines_list = []
+                # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=1)
 
-                psum = '0'
+                # psum = '0'
 
-                if lines is not None:
-                    for points in lines:
-                        x1, y1, x2, y2 = points[0]
-                        cv2.line(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                        lines_list.append([(x1, y1), (x2, y2)])
-                        scale_len = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-                        um_pix = float(psum) / scale_len
-                else:
-                    um_pix = 1
-                    psum = '0'
+                # if lines is not None:
+                #     for points in lines:
+                #         x1, y1, x2, y2 = points[0]
+                #         cv2.line(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                #         lines_list.append([(x1, y1), (x2, y2)])
+                #         scale_len = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                #         um_pix = float(psum) / scale_len
+                # else:
+                #     um_pix = 1
+                #     psum = '0'
 
                 # Detect text in the image
                 reader = easyocr.Reader(["en"])
@@ -734,10 +734,6 @@ def run_inference(dataset_name, output_dir, visualize=False, threshold=0.65):
                 else:
                     um_pix = 1
                     psum = "0"
-
-                # um_per_pixel, real_um = detect_scale_bar(im)
-                # um_pix = um_per_pixel
-                # psum = real_um
 
                 # end new here #######################
 
